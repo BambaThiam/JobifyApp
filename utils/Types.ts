f911ff26371666b749a30e3cd00c62d8043b186b
DayOneAI <bambathiam88@gmail.com>
2024-04-13 21:27:27 +0200

    feat: ✨Types #9

---
+++ utils/Types.ts
@@ -0,0 +1,39 @@
+import * as z from 'zod';
+
+export type JobType = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  clerkId: string;
+  position: string;
+  company: string;
+  location: string;
+  status: string;
+  mode: string;
+}
+
+//create enum for job status with the values 'applied', 'interview', 'offer', and 'rejected'
+export enum JobStatus {
+  Applied = 'applied',
+  Interview = 'interview',
+  Offer = 'offer',
+  Rejected = 'rejected',
+}
+
+export enum JobMode {
+  FullTime = 'full-time',
+  PartTime = 'part-time',
+  Internship = 'internship',
+}
+
+// Enums in TypeScript are a special type that allows you to define a set of named constants. They can be numeric or string-based.
+
+export const createAndEditJobSchema = z.object({
+  position: z.string().min(2, { message: 'Position must be at least 2 characters.' }),
+  company: z.string().min(2, { message: 'Company must be at least 2 characters.' }),
+  location: z.string().min(2, { message: 'Location must be at least 2 characters.' }),
+  status: z.nativeEnum(JobStatus),
+  mode: z.nativeEnum(JobMode),
+})
+
+export type CreateAndEditJobSchema = z.infer<typeof createAndEditJobSchema>
